53cc36635d21d53db3909ee10e8248d7468210c1
Omer Lev <omer.h.lev@gmail.com>
2024-02-04 12:53:13 +0200

    added pytest command to choose browser at runtime

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -3,7 +3,9 @@ import time
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as chromeService
+from selenium.webdriver.edge.service import Service as edgeService
+from selenium.webdriver.firefox.service import Service as firefoxService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 
@@ -11,12 +13,30 @@ from selenium.webdriver.support.wait import WebDriverWait
 import logging
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="chrome firefox or edge"
+    )
+
+
 @pytest.fixture(scope="class")
 def setup(request):
-    service_obj = Service("/Users/omerh/Documents/chromedriver-win64/chromedriver.exe")
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(service=service_obj, options=options)
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        service_obj = chromeService("/Users/omerh/Documents/chromedriver-win64/chromedriver.exe")
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(service=service_obj, options=options)
+    elif browser_name == "firefox":
+        service_obj = firefoxService("/Users/omerh/Documents/geckodriver-v0.33.0-win64/geckodriver.exe")
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(service=service_obj, options=options)
+    elif browser_name == "edge":
+        service_obj = edgeService("/Users/omerh/Documents/edgedriver_win64/msedgedriver.exe")
+        options = webdriver.EdgeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Edge(service=service_obj, options=options)
 
     driver.implicitly_wait(5)
     driver.get("https://www.nytimes.com/crosswords")
