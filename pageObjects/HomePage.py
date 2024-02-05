ec2468375986acd4fe0b09e087b11be3b9d47cb3
Omer Lev <omer.h.lev@gmail.com>
2024-02-05 14:08:34 +0200

    Reorganized HomePage and added many new objects

--- pageObjects/HomePage.py
+++ pageObjects/HomePage.py
@@ -6,26 +6,45 @@ class HomePage:
     def __init__(self, driver):
         self.driver = driver
 
-    dashboard_games_list = (By.CSS_SELECTOR, ".hub-game-card__cover")
-    featured_rotating_game = (By.CSS_SELECTOR, ".hub-welcome__section.alternate")
-    featured_games_permanent_list = (By.CSS_SELECTOR, ".featured")
-    wordplay_link = (By.CSS_SELECTOR, ".hub-wordplay-link")
+    # HEAD
     logo = (By.CSS_SELECTOR, ".pz-nav__logo")
     navigation_menu = (By.CSS_SELECTOR, ".pz-nav__hamburger-box")
     subscribe_buttons_list = (By.CSS_SELECTOR, ".pz-nav__button.default.js-nav-subscribe.pz-hide-sub")
-                                                            #First one is in the dashboard,
-                                                            # seconed in the navigation collapsable menu
-    login_buttons_list = (By.CSS_SELECTOR, ".js-nav-login") #First one is in the dashboard,
-                                                            # seconed in the navigation collapsable menu
+    # First one is in the dashboard
+    # seconed in the navigation collapsable menu
+    login_buttons_list = (By.CSS_SELECTOR, ".js-nav-login")  # First one is in the dashboard,
+    # seconed in the navigation collapsable menu
+    # NAVIGATION MENU
     navigation_menu_buttons_list = (By.CSS_SELECTOR, ".js-nav-tracker.pz-nav-drawer__link")
 
-    #Finish page objects
+    # FEATURED
+    featured_rotating_game = (By.CSS_SELECTOR, ".hub-welcome__section.alternate")
+    featured_games_permanent_list = (By.CSS_SELECTOR, ".featured")
+    wordplay_link = (By.CSS_SELECTOR, ".hub-wordplay-link")
+
+    # DASHBOARD
+    dashboard_games_list = (By.CSS_SELECTOR, ".hub-game-card__cover")
+    dashboard_header = (By.CSS_SELECTOR, ".section__header")
+
+    # FEATURED ARTICLE
+    featured_article_header = (By.CSS_SELECTOR, "h3[class='hub-section-header']")
+    feature_article = (By.CSS_SELECTOR, ".hub-guide-promo-card.js-hub-tracker")
+
+    # ARCHIVE
+    archive_header = (By.CSS_SELECTOR, "h2[class='hub-section-header']")
+    archive_puzzles_list = (By.CSS_SELECTOR, ".island.action-play.standard.puzzleInfo.js-hub-tracker")
+    # Should be 4
+    more_button = (By.LINK_TEXT, "MORE")
+
+    # BOTTOM
+    # I think these are unnecessary to add, but if in the future I want those tested too here would
+    # be the place to add them
+
+    # Finish page objects
 
     def get_dashboard_games_list(self):
         return self.driver.find_elements(*HomePage.dashboard_games_list)
 
+    # Finish all get functions
 
-    #Finish all get functions
-
-
-    #Create get page functions
+    # Create get page functions
