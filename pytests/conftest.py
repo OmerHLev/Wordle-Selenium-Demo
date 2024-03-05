f28ce18e969f3464142476da6e7246bd2e1f9807
Omer Lev <omer.h.lev@gmail.com>
2024-03-05 08:26:07 +0200

    added screenshots to html.report on test fail

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from utilities.Logger import LoggerClass
 from contextlib import contextmanager
 from utilities import Constants
-
+driver = None
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="chrome", help="chrome, firefox, edge or chrome_view"
@@ -17,6 +17,7 @@ def pytest_addoption(parser):
 
 @contextmanager
 def setup_base(request):
+    global driver
     browser_name = request.config.getoption("browser_name")
     driver = invoke_browser(browser_name)
     wait = invoke_waits(driver,Constants.IMPLICIT_WAIT_TIME,Constants.EXPLICIT_WAIT_TIME)
@@ -61,3 +62,25 @@ def invoke_browser(browser_name):
 def invoke_waits(driver,imp_wait,exp_wait):
     driver.implicitly_wait(imp_wait)
     return WebDriverWait(driver, exp_wait)
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
