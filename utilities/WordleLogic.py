0ff8bee64554b1f9ca3389e18e8fd825bd676d04
Omer Lev <omer.h.lev@gmail.com>
2024-03-06 13:15:36 +0200

    redid WordleLogic, I think it actaully works this time!

--- utilities/WordleLogic.py
+++ utilities/WordleLogic.py
@@ -3,79 +3,55 @@ import string
 from utilities import Constants
 
 class WordleLogic:
-    def __init__(self):
-        self.untested_letters = ([*string.ascii_uppercase])
-        self.cells = []
-        for cell_position in range(Constants.WORD_LENGTH): self.cells.append(WordleCell(cell_position))
-        self.present_letters =[]
-        self.known_letters = []
+    def __init__(self,true_answer):
+        self.answer = true_answer
+        self.unique_letters_answer = list(set(self.answer))
 
-    def new_word(self, word, clues):
-        position = 0
-        for letter in word:
-            self.new_clue(letter, position, clues[position])
-            position += 1
-
-    def exclusive_append(self,curr_list, item):
-        if item not in curr_list:
-            curr_list.append(item)
-        return curr_list
-
-    def new_clue(self, letter,position,clue):
-        if letter in self.untested_letters:
-            self.untested_letters.remove(letter)
-        match clue:
-            case "absent":
-                if letter in self.cells[position].correct:
-                    raise Exception("Letter said to be absent but is registered as correct")
-                self.cells[position].incorrect = self.exclusive_append(self.cells[position].incorrect, letter)
-
-            case "elsewhere":
-                if letter in self.cells[position].correct:
-                    raise Exception("Letter said to be elsewhere but is registered as correct")
-                self.present_letters = self.exclusive_append(self.present_letters, letter)
-                self.cells[position].incorrect = self.exclusive_append(self.cells[position].incorrect, letter)
-            case "hit":
-                if letter in self.cells[position].incorrect:
-                    raise Exception("Letter said to be correct but is registered as incorrect")
-                self.known_letters = self.exclusive_append(self.known_letters, letter)
-                self.present_letters = self.exclusive_append(self.present_letters, letter)
-                self.cells[position].correct = self.exclusive_append(self.cells[position].correct, letter)
-    def check_word_to_guess(self,word):
-        position = 0
-        for letter in word:
-            if letter in self.untested_letters or letter in self.present_letters:
-                if letter not in self.cells[position].incorrect:
-                    pass
+    def is_new_word_clues_valid(self,word,clues):
+        unique_letters = list(set(word))
+        for letter_unique in unique_letters:
+            if letter_unique not in self.unique_letters_answer:
+                pos = 0
+                for letter in word:
+                    if letter == letter_unique and clues[pos] != "absent":
+                        return False
+                    pos += 1
+            elif self.answer.count(letter_unique) >= word.count(letter_unique):
+                pos = 0
+                for letter in word:
+                    if letter == letter_unique:
+                        if self.answer[pos] != letter_unique:
+                            if clues[pos] != "present":
+                                return False
+                        elif clues[pos] != "correct":
+                            return False
+                    pos += 1
+            elif self.answer.count(letter_unique) < word.count(letter_unique):
+                pos = 0
+                positive_clue_count = 0
+                for letter in word:
+                    if letter == letter_unique:
+                        if clues[pos] == "correct" or clues[pos] == "present":
+                            positive_clue_count += 1
+                    pos += 1
+                if positive_clue_count != self.answer.count(letter_unique):
+                    return False
                 else:
-                     return False
-            else:
-                 return False
-            position += 1
+                    pos = 0
+                    for letter in word:
+                        if letter == letter_unique:
+                            if self.answer[pos] == letter_unique and clues[pos] != "correct":
+                                return False
+                        pos += 1
         return True
+logic = WordleLogic("TEARY")
+print(logic.is_new_word_clues_valid("AAHEA", ["present", "absent", "absent", "present", "absent"]))
+print(logic.is_new_word_clues_valid("AATEA", ["present", "absent", "present", "present", "absent"]))
+print(logic.is_new_word_clues_valid("TAHEA", ["correct", "absent", "absent", "present", "present"]))
+print(logic.is_new_word_clues_valid("AAHEA", ["present", "correct", "absent", "present", "absent"]))
+print(logic.is_new_word_clues_valid("GEEKY", ["absent", "correct", "absent", "absent", "correct"]))
+print(logic.is_new_word_clues_valid("AAAAA", ["absent", "absent", "correct", "absent", "absent"]))
 
 
-class WordleCell:
-    def __init__(self,position):
-        self.position = position
-        self.incorrect =[]
-        self.correct = []
 
 
-def testing_temporary():
-    wordle = WordleLogic()
-    wordle.new_word("RATES",["absent", "absent", "absent", "absent", "absent"])
-    wordle.new_word("POUND", ["absent", "absent", "elsewhere", "absent", "hit"])
-    wordle.new_word("MILKY", ["absent", "elsewhere", "elsewhere", "absent", "absent"])
-    wordle.new_word("MILKY", ["absent", "elsewhere", "elsewhere", "absent", "absent"])
-    wordle.new_word("GUILD", ["absent", "hit", "hit", "hit", "hit"])
-    print(wordle.check_word_to_guess("BUILD"))
-    print(wordle.check_word_to_guess("FIGHT"))
-    print(wordle.untested_letters)
-    print(wordle.known_letters)
-    print(wordle.present_letters)
-    print(wordle.cells[3].incorrect)
-    print(wordle.cells[3].correct)
-    print(wordle.cells[4].incorrect)
-    print(wordle.cells[4].correct)
-testing_temporary()
